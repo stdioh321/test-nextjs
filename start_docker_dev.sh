92e5cbfea9a3715550b81848581c1e80f9e84cc8
Henrique <henrique.dias@gruponewway.com.br>
2023-10-18 13:23:16 -0300

    chore: update docker-compose

--- start_docker_dev.sh
+++ start_docker_dev.sh
@@ -1,10 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 
 docker_compose_file="./infra/docker-compose-dev.yml"
-docker_build_flag="-b"
+compose_command="docker-compose"
+build=false
 
-if [[ "$*" == *"$docker_build_flag"* ]]; then
-    docker-compose -f "$docker_compose_file" build --no-cache
+if ! command -v "$compose_command" &> /dev/null; then
+  if command -v "docker compose" &> /dev/null; then
+    compose_command="docker compose"
+  else
+    echo "Error: Neither 'docker-compose' nor 'docker compose' command found."
+    exit 1
+  fi
 fi
 
-docker-compose -f "$docker_compose_file" up
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -f|--file)
+      docker_compose_file="$2"
+      shift 2
+      ;;
+    -b|--build)
+      build=true
+      shift
+      ;;
+    *)
+      echo "Invalid option: $1" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ "$build" = true ]; then
+  "$compose_command" -f "$docker_compose_file" build --no-cache
+fi
+
+"$compose_command" -f "$docker_compose_file" up
